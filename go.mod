51a6ca44367ab77dd80b785918fe116fcb3b2e8f
cadullms <cadullms@github.com>
2019-10-13 22:19:25 +0000

    first steps config var handling first steps mongo

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module dev.azure.com/cadullms/GoPlayground/_git/go-mongo
 go 1.13
 
 require (
-	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
+	github.com/spf13/viper v1.4.0
 	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
 	github.com/xdg/stringprep v1.0.0 // indirect
 	go.mongodb.org/mongo-driver v1.1.2
--- main.go
+++ main.go
@@ -1,35 +1,58 @@
 package main
 
+// https://medium.com/@bnprashanth256/reading-configuration-files-and-environment-variables-in-go-golang-c2607f912b63
 // https://www.mongodb.com/blog/post/mongodb-go-driver-tutorial
 
 import (
-    "context"
-    "fmt"
-    "log"
+	// "context"
+	"fmt"
+	// "log"
 
-    // "go.mongodb.org/mongo-driver/bson"
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
+	// "go.mongodb.org/mongo-driver/bson"
+	// "go.mongodb.org/mongo-driver/mongo"
+	// "go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/spf13/viper"
 )
 
+type Session struct {
+	UserId   string
+	LessonId string
+}
+
+type Lesson struct {
+	LessonId string
+}
+
 func main() {
-	fmt.Println("Hello World 2")
-	// Set client options
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 
-	// Connect to MongoDB
-	client, err := mongo.Connect(context.TODO(), clientOptions)
-
-	if err != nil {
-		log.Fatal(err)
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Printf("Error reading config file, %s", err)
 	}
 
-	// Check the connection
-	err = client.Ping(context.TODO(), nil)
+	theValue := viper.GetString("SESSION_URL")
+	fmt.Println("the value is", theValue)
 
-	if err != nil {
-		log.Fatal(err)
-	}
+	// fmt.Println("Hello World 2")
+	// // Set client options
+	// clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 
-	fmt.Println("Connected to MongoDB!")
+	// // Connect to MongoDB
+	// client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// // Check the connection
+	// err = client.Ping(context.TODO(), nil)
+
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// fmt.Println("Connected to MongoDB!")
 }
